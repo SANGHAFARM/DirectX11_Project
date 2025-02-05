1a615b10bb9924b2430310f2ea40090e52eca82d
Sang Ha <sangha972@naver.com>
2025-02-05 17:35:05 +0900

    창(Window), 렌더러, 엔진 클래스 구현

---
+++ Engine/Core/Engine.h
@@ -0,0 +1,40 @@
+#pragma once
+
+#include <memory>
+#include <Windows.h>
+#include <string>
+
+#include "../Core/Type.h"
+
+namespace Blue
+{
+	class Window;
+	class Renderer;
+
+	// 엔진 클래스
+	class Engine
+	{
+	public:
+		Engine(uint32 width, uint32 height, const std::wstring& title, HINSTANCE hInstance);
+		virtual ~Engine();
+
+		// 엔진 실행 함수
+		void Run();
+
+		// 윈도우 메시지 처리 루프
+		static LRESULT CALLBACK WindowProc(HWND hande, UINT message, WPARAM wparam, LPARAM lparam);
+
+		// 싱글톤 접근 함수
+		static Engine& Get();
+
+	protected:
+		// 창 객체
+		std::shared_ptr<Window> window;
+
+		// 렌더러 객체
+		std::shared_ptr<Renderer> renderer;
+
+		// 싱글톤 객체
+		static Engine* instance;
+	};
+}
